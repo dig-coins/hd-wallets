c76420f3a51ef37608edf1f4758e4afd9765ee59
patdz <patdz@ymipro.com>
2024-04-01 16:56:48 +0800

    u

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,5 @@ rm -rf $dest
 mkdir -p $dest
 
 go build -ldflags=-linkmode=internal -o $dest/hd-wallet cmd/hd-wallet/main.go
-
-
+GOARCH=amd64 GOOS=windows go build -ldflags=-linkmode=internal -o $dest/hd-wallet.exe cmd/hd-wallet/main.go
 
